d3211f79a562e160aa3b5e25b0a4de28acd73c5b
PengZai <649365461@qq.com>
2025-02-24 00:37:17 +0000

    optimize the coordinate of evaluation

--- evaluation_results/evo_eval.sh
+++ evaluation_results/evo_eval.sh
@@ -1,14 +1,14 @@
 
 DATASET="BotanicGarden"
-DIR="1005_07"
+DIR="1018_00"
+TEST_DIR="test_0"
 
-ESITMATED_FOLDER="vinsfusion_stereo_inertial_${DIR}_img10hz600p"
+ESITMATED_FOLDER="vinsfusion_stereo_for_${DIR}_img10hz600p"
 REFERENCE="${DIR}_GT_output.txt"
 
-evo_rpe tum $DATASET/$DIR/${ESITMATED_FOLDER}/${ESITMATED_FOLDER}_vio.csv $DATASET/$DIR/$REFERENCE  > $DATASET/$DIR/${ESITMATED_FOLDER}/rpe_${ESITMATED_FOLDER}_vio.log
-evo_ape tum $DATASET/$DIR/${ESITMATED_FOLDER}/${ESITMATED_FOLDER}_vio.csv $DATASET/$DIR/$REFERENCE  > $DATASET/$DIR/${ESITMATED_FOLDER}/ape_${ESITMATED_FOLDER}_vio.log
-evo_ape tum $DATASET/$DIR/${ESITMATED_FOLDER}/${ESITMATED_FOLDER}_vio.csv $DATASET/$DIR/$REFERENCE --align --pose_relation trans_part > $DATASET/$DIR/${ESITMATED_FOLDER}/ate_${ESITMATED_FOLDER}_vio.log
+evo_rpe tum $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/${ESITMATED_FOLDER}_vio_coordinate_aligned.txt $DATASET/$DIR/$REFERENCE  > $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/rpe_${ESITMATED_FOLDER}_vio_coordinate_aligned.log
+evo_ape tum $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/${ESITMATED_FOLDER}_vio_coordinate_aligned.txt $DATASET/$DIR/$REFERENCE  > $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/ape_${ESITMATED_FOLDER}_vio_coordinate_aligned.log
+evo_ape tum $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/${ESITMATED_FOLDER}_vio_coordinate_aligned.txt $DATASET/$DIR/$REFERENCE --align --pose_relation trans_part > $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/ate_${ESITMATED_FOLDER}_vio_coordinate_aligned.log
 
-
-evo_traj tum $DATASET/$DIR/${ESITMATED_FOLDER}/${ESITMATED_FOLDER}_vio.csv --ref=$DATASET/$DIR/$REFERENCE -p --plot_mode=xyz --save_plot $DATASET/$DIR/${ESITMATED_FOLDER}/3d  
-evo_traj tum $DATASET/$DIR/${ESITMATED_FOLDER}/${ESITMATED_FOLDER}_vio.csv --ref=$DATASET/$DIR/$REFERENCE -p --plot_mode=xy --save_plot $DATASET/$DIR/${ESITMATED_FOLDER}/2d 
+evo_traj tum $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/${ESITMATED_FOLDER}_vio_coordinate_aligned.txt --ref=$DATASET/$DIR/$REFERENCE -p --plot_mode=xyz --save_plot $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/3d  
+evo_traj tum $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/${ESITMATED_FOLDER}_vio_coordinate_aligned.txt --ref=$DATASET/$DIR/$REFERENCE -p --plot_mode=xy --save_plot $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/2d 
