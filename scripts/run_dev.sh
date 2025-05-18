7c5de16b6d30fb36a0537410af81432b9873f885
PengZai <649365461@qq.com>
2025-05-18 19:09:39 +0100

    modified scripts/run_dev.sh

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-
+PROJECT_NAME="vins-fusion"
 IMAGE_NAME="vins-fusion:noetic-ros"
 DATA_PATH="/media/${USER}/zhipeng_usb/datasets"
+DATA_PATH2="/media/${USER}/zhipeng_8t/datasets"
 # Pick up config image key if specified
 if [[ ! -z "${CONFIG_DATA_PATH}" ]]; then
     DATA_PATH=$CONFIG_DATA_PATH
 fi
 
 
-PROJECT_DIR=$(pwd)
-PROJECT_NAME=$(basename "$PWD")
+
 
 
 docker build -t $IMAGE_NAME -f "${HOME}/vscode_projects/VINS-Fusion/catkin_ws/src/VINS-Fusion/Dockerfile_for_dev" .
@@ -25,6 +25,7 @@ docker run \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -v ${HOME}/vscode_projects/VINS-Fusion/catkin_ws:/root/catkin_ws \
     -v ${DATA_PATH}:/root/datasets \
+    -v ${DATA_PATH2}:/root/datasets2 \
     --privileged \
     --cap-add sys_ptrace \
     --runtime=nvidia \
